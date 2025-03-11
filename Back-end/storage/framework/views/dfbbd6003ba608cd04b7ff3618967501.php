31dbf5dcbbc07a15da158526ad3f22beb0c8847d
Haviprasath <haviprasaths@gmail.com>
2025-03-11 14:55:46 +0530

    Added backend files

---
+++ Back-end/storage/framework/views/dfbbd6003ba608cd04b7ff3618967501.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('content'); ?>
+    <div>
+        <h1>hi havi</h1>
+    </div>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts/master', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Laravel\workspace\job_findProject\resources\views/Home.blade.php ENDPATH**/ ?>
