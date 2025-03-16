06a5fa514c28d5aae9312a34deacd97c036d4440
kumarimanoja4@gmail.com <kumarimanoja4@gmail.com>
2025-03-16 22:46:46 +0530

    Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div className="p-4">
+      <button className="bg-gray-200 text-gray-800 px-4 py-2 rounded">Logout</button>
+    </div>
+  );
+};
+
+export default Footer;
