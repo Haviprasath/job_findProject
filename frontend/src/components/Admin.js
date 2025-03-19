e8e21286a0aa0faab3d0ed1abd81a3eaca04ead6
RagavanKulachchelvam <ragavankulachchelvam@gmail.com>
2025-03-19 14:57:35 +0530

    Sidebar for Admin page

---
+++ frontend/src/components/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidebar1 from './layouts/sidebar'
+
+const Admin = () => {
+  return (
+    <div className='dashboard'>
+        <Sidebar1 />
+    </div>
+  )
+}
+
+export default Admin
