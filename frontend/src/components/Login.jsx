e40f03c1c5f72c533c3e164dfc0270191d3b3a19
sajahanmasma <sajahanmasma@gmail.com>
2025-03-19 22:30:44 +0530

    day2

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,30 @@
+import './css/login.css';
+import image1 from '../assets/img/man.png';
+
+
+
 export default function Login()
 {
     return(
         <>
-        <h1>masma</h1>
+        <div className='loginBack'>
+        <div className='outlineBorder'>
+        <form className='loginForm'>
+          <h1>LOGIN</h1>
+          <p>JOBie</p>
+          <input type='text' className='fa' placeholder='&#xf007; Username'/><br/>
+          <input type='password' className='fa' placeholder='&#xf023; Password'/><br/>
+          <button className='loginButton'>Login Now</button>
+          <a href='/register' className='labelLogin'><p><label>SignUp</label>  Here</p></a>
+        </form>
+        <span className='logoareaLogin'>
+          <div className='blurArea'>
+              
+          </div>
+          <img src={image1} className='logoVanni'/>
+        </span>
+      </div>
+        </div>
         </>
     )
 }
